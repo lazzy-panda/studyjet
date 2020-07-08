578c0738719192aaaab56364345a243b1935c8b7
Kirill Moroozov <grifx.design@gmail.com>
2020-07-08 14:09:13 +0300

    3 screen + Objections start page layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {ObjectionsComponent} from './courses/objections/objections.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'courses/objections',
+    component: ObjectionsComponent
   }
 ];
 @NgModule({
---
+++ src/app/courses/objections/objections.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-objections',
+  templateUrl: './objections.component.html',
+  styleUrls: ['./objections.component.scss'],
+})
+export class ObjectionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
